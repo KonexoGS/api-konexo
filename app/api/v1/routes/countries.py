01e8fde8382b43feed7d3668dbf5ff8656e8eabd
Scacchetti <luiscacchetti07@gmail.com>
2025-11-16 06:27:06 -0300

    feat: rota com busca de todos os países

--- app/api/v1/routes/countries.py
+++ app/api/v1/routes/countries.py
@@ -40,7 +40,7 @@ def get_country_base_nfo(country_iso_code: str):
         raise HTTPException(status_code=500, detail={"Unexpected Erro": "Ocorreu um erro inesperado na busca pelo país"})
     
 @router.get('/info/detail/{country_iso_code}', name='Procure sobre os detalhes de um determinado país especificado', response_model=IndicatorsModelSchema | None)
-def get_pib_from_country(info: Literal["PIB", "Populacao", "Desigualdade(GINI)", "Trabalhadores Agro", "ForcaTotal Trabalho"], country_iso_code: str, year: Annotated[str | None, Query(max_length=5)] = None):
+def get_details_from_country(info: Literal["PIB", "Populacao", "Desigualdade(GINI)", "Trabalhadores Agro", "ForcaTotal Trabalho"], country_iso_code: str, year: Annotated[str | None, Query(max_length=5)] = None):
     try:
         if len(country_iso_code) > 3 or len(country_iso_code) <= 1:
             raise HTTPException(status_code=400, detail="O código ISO do país deve conter de duas a três caractéres.")
@@ -63,6 +63,37 @@ def get_pib_from_country(info: Literal["PIB", "Populacao", "Desigualdade(GINI)",
         return FormatCountry.format_to_pib_model(country[1])
         
 
+    except TypeError as type_ex:
+        raise HTTPException(status_code=400, detail=Translator.translatePlainText(type_ex.__str__()))
+    except HTTPException as http_ex:
+        raise http_ex
+    except ResponseValidationError as resp_ex:
+        raise HTTPException(detail=resp_ex.__str__(), status_code=500)
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="Ocorreu um erro inesperado na busca pelo país")
+    
+@router.get('/info/detail/s/all', name='Procure sobre os detalhes de um determinado país especificado', response_model=None)
+def get_details_from_all_country(info: Literal["PIB", "Populacao", "Desigualdade(GINI)", "Trabalhadores Agro", "ForcaTotal Trabalho"], year: Annotated[str | None, Query(max_length=5)] = None):
+    try:
+        
+        indicator = get_indicator_info(info)
+        
+        uri = f'{world_bank_url}/country/all/indicators/{indicator}?format=json'
+        if year and year.isnumeric():
+            format_year: int = int(year)
+            if format_year <= datetime.now().year:
+                uri += f'&date={year}'
+
+        response: Response = request('GET', uri)
+        country: Any = response.json()
+
+        if len(country) <= 1:
+            error: str = country[0]['message'][0]['value']
+            raise HTTPException(status_code=404, detail=Translator.translatePlainText(error))
+        
+        return country
+        
     except TypeError as type_ex:
         raise HTTPException(status_code=400, detail=Translator.translatePlainText(type_ex.__str__()))
     except HTTPException as http_ex:
