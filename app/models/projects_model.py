2b594a452c94ff3f03b3ab03856dc8d640f2d596
Scacchetti <luiscacchetti07@gmail.com>
2025-11-20 02:44:04 -0300

    fix: Correção nas tipagem do modelo

--- app/models/projects_model.py
+++ app/models/projects_model.py
@@ -4,16 +4,16 @@ from pydantic import Base64Str, BaseModel
 
 
 class ProjectsModel(BaseModel):
-    friendly_code: str
+    friendly_code: str | None
     owner_id: str
     name: str
     short_description: str
     full_description: str
     category: List[str]
-    has_github: bool
+    has_github: bool = False
     developers_id: List[str]
-    banner_photo: Base64Str
-    is_deleted: bool
+    banner_photo: Base64Str | None
+    is_deleted: bool = False
 
 class ProjectsResultModel(ProjectsModel):
     _id: str
